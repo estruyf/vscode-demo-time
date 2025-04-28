73c55002622a3c719c3a0fc9a929d79a788566d9
Elio Struyf <elio@struyfconsulting.be>
2025-04-28 14:07:30 +0200

    Update scripts

--- .github/scripts/beta-release.mjs
+++ .github/scripts/beta-release.mjs
@@ -1,6 +1,6 @@
 import * as core from '@actions/core';
-import * as fs from 'fs';
-import * as path from 'path';
+import { readFile, writeFile } from "fs/promises";
+import { join, resolve } from 'path';
 
 const packageContents = await readFile('package.json', 'utf8');
 const packageJson = JSON.parse(packageContents);
@@ -12,7 +12,7 @@ packageJson.version = `${version[0]}.${version[1]}.${process.argv[
 
 await core.summary.addHeading(`Version info`).addRaw(`Version: ${packageJson.version}`).write();
 
-fs.writeFileSync(
-  path.join(path.resolve('.'), 'package.json'),
+await writeFile(
+  join(resolve('.'), 'package.json'),
   JSON.stringify(packageJson, null, 2)
 );
