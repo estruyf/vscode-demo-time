d916276ce872420b9e3d2bbc1840b325924cea3c
Elio Struyf <elio@struyfconsulting.be>
2025-08-25 12:45:17 +0200

    fix: Improve error handling and HTML loading for webview; streamline production mode logic and enhance development mode structure

--- apps/vscode-extension/src/utils/getWebviewHtml.ts
+++ apps/vscode-extension/src/utils/getWebviewHtml.ts
@@ -8,56 +8,85 @@ import { WebviewHtml } from '../webview';
 export const getWebviewHtml = async (type: WebviewType, webview: Webview) => {
   Logger.info(`Loading webview HTML for type: ${type}`);
 
-  const extensions = Extension.getInstance();
-  const extensionUri = extensions.extensionUri;
-  let webviewHtml = WebviewHtml;
+  const extension = Extension.getInstance();
 
-  if (!webviewHtml) {
-    const webviewPath = Uri.joinPath(extensionUri, 'webviews', 'index.html');
+  if (extension.isProductionMode) {
+    const extensionUri = extension.extensionUri;
+    let webviewHtml = WebviewHtml;
 
-    if (!(await fileExists(webviewPath))) {
-      Notifications.error('Webview HTML file not found.');
-      return;
+    if (!webviewHtml) {
+      const webviewPath = Uri.joinPath(extensionUri, 'webviews', 'index.html');
+
+      if (!(await fileExists(webviewPath))) {
+        Notifications.error('Webview HTML file not found.');
+        return;
+      }
+
+      try {
+        const html = await readFile(webviewPath);
+        if (!html) {
+          Notifications.error('Failed to read webview HTML file.');
+          return;
+        }
+
+        webviewHtml = html;
+      } catch (error) {
+        Notifications.error('Error loading webview HTML file.');
+        return;
+      }
     }
 
+    const webviewWsUrl = getWebviewWorkspaceUrl(webview, '');
+    const webviewExtUrl = getWebviewExtensionUrl(webview);
+
     try {
-      const html = await readFile(webviewPath);
-      if (!html) {
+      if (!webviewHtml) {
         Notifications.error('Failed to read webview HTML file.');
         return;
       }
 
-      webviewHtml = html;
+      const baseUrl = `${webviewExtUrl}/webviews/`;
+
+      let patchedHtml = webviewHtml || '';
+      if (patchedHtml.includes('<base href="/" />')) {
+        patchedHtml = patchedHtml.replace('<base href="/" />', `<base href="${baseUrl}" />`);
+      }
+
+      patchedHtml = patchedHtml.replace(
+        `id="root"`,
+        `id="root" data-view-type="${type}" data-webview-url="${webviewWsUrl}" data-extension-url="${webviewExtUrl}"`,
+      );
+
+      return patchedHtml;
     } catch (error) {
       Notifications.error('Error loading webview HTML file.');
       return;
     }
-  }
+  } else {
+    // DEVELOPMENT MODE
+    return `
+        <!doctype html>
+        <html lang="en">
+          <head>
+            <script type="module">
+              import RefreshRuntime from "http://localhost:5173/@react-refresh"
+              RefreshRuntime.injectIntoGlobalHook(window)
+              window.$RefreshReg$ = () => {}
+              window.$RefreshSig$ = () => (type) => type
+              window.__vite_plugin_react_preamble_installed__ = true
+            </script>
 
-  const webviewWsUrl = getWebviewWorkspaceUrl(webview, '');
-  const webviewExtUrl = getWebviewExtensionUrl(webview);
+            <script type="module" src="http://localhost:5173/@vite/client"></script>
 
-  try {
-    if (!webviewHtml) {
-      Notifications.error('Failed to read webview HTML file.');
-      return;
-    }
-
-    const baseUrl = `${webviewExtUrl}/webviews/`;
-
-    let patchedHtml = webviewHtml || '';
-    if (patchedHtml.includes('<base href="/" />')) {
-      patchedHtml = patchedHtml.replace('<base href="/" />', `<base href="${baseUrl}" />`);
-    }
-
-    patchedHtml = patchedHtml.replace(
-      `id="root"`,
-      `id="root" data-view-type="${type}" data-webview-url="${webviewWsUrl}" data-extension-url="${webviewExtUrl}"`,
-    );
-
-    return patchedHtml;
-  } catch (error) {
-    Notifications.error('Error loading webview HTML file.');
-    return;
+            <meta charset="UTF-8" />
+            <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Demo Time Config Editor</title>
+          </head>
+          <body>
+            <div id="root" data-view-type="${type}"></div>
+            <script type="module" src="http://localhost:5173/src/main.tsx"></script>
+          </body>
+        </html>`;
   }
 };
