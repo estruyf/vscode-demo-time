7fed5b253d74844a37b80ed53d12f0bc7b7428cd
Elio Struyf <elio@struyfconsulting.be>
2025-08-26 21:11:51 +0200

    fix: Update build scripts and dependencies in package.json and WebviewHtml.ts

--- apps/vscode-extension/src/webview/WebviewHtml.ts
+++ apps/vscode-extension/src/webview/WebviewHtml.ts
@@ -5,8 +5,8 @@ export const WebviewHtml = `<!doctype html>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Demo Time Webview</title>
-    <script type="module" crossorigin src="./assets/index-HxuaE083.js"></script>
-    <link rel="stylesheet" crossorigin href="./assets/index-BFFzwx-q.css">
+    <script type="module" crossorigin src="./assets/index-tM0esR6J.js"></script>
+    <link rel="stylesheet" crossorigin href="./assets/index-DLGA4Yij.css">
   </head>
   <body>
     <div id="root"></div>
