4ae8d7b45d4e10bb835a92edaa7edf7486ecb7cb
Elio Struyf <elio@struyfconsulting.be>
2025-08-29 17:29:52 +0200

    eslint updates

--- apps/mcp/eslint.config.mjs
+++ apps/mcp/eslint.config.mjs
@@ -1,8 +1,12 @@
 import baseConfig from '../../eslint.config.mjs';
 
+const tsconfigRootDir = path.dirname(fileURLToPath(import.meta.url));
+
+
 export default [
   ...baseConfig,
   {
     ignores: ['out-tsc', 'node_modules'],
+    tsconfigRootDir
   },
 ];
--- apps/vscode-extension/eslint.config.mjs
+++ apps/vscode-extension/eslint.config.mjs
@@ -1,8 +1,11 @@
 import baseConfig from '../../eslint.config.mjs';
 
+const tsconfigRootDir = path.dirname(fileURLToPath(import.meta.url));
+
 export default [
   ...baseConfig,
   {
     ignores: ['out-tsc', 'node_modules'],
+    tsconfigRootDir
   },
 ];
