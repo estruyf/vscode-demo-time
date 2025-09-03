6caf863ad554e0917f50aad485ec86b80c991ed0
Elio Struyf <elio@struyfconsulting.be>
2025-09-03 17:34:48 +0200

    Create improved demo overview with collapsible sections

--- apps/webviews/src/components/overview/index.ts
+++ apps/webviews/src/components/overview/index.ts
@@ -2,3 +2,7 @@ export * from './DemoFlowNode';
 export * from './DemoScriptOverview';
 export * from './FlowConnector';
 export * from './StepFlowNode';
+export * from './DemoFilesOverview';
+export * from './DemoFileSection';
+export * from './DemoFileCard';
+export * from './DemoFileMetadata';
---
+++ apps/webviews/src/types/IDemoTimeSettings.ts
@@ -0,0 +1,2 @@
+// Re-export from common package for consistency
+export type { IDemoTimeSettings } from '@demotime/common';
