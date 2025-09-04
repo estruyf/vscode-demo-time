c1b3d13f64efc3329a4c89183ef994b75570ab86
Elio Struyf <elio@struyfconsulting.be>
2025-09-04 14:36:22 +0200

    Remove unused imports and tsconfigRootDir from ESLint configuration

--- packages/common/eslint.config.mjs
+++ packages/common/eslint.config.mjs
@@ -1,13 +1,8 @@
 import baseConfig from '../../eslint.config.mjs';
-import { fileURLToPath } from 'url';  
-import path from 'path';
-
-const tsconfigRootDir = path.dirname(fileURLToPath(import.meta.url));
 
 export default [
   ...baseConfig,
   {
     ignores: ['out-tsc', 'node_modules'],
-    tsconfigRootDir
   },
 ];
