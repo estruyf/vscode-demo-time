98f7246bc1311c9dd7eb2c55c007779a44891630
Elio Struyf <elio@struyfconsulting.be>
2026-01-19 17:02:38 +0100

    fix: update jest configuration to include moduleNameMapper for p-limit and yocto-queue

--- packages/common/jest.config.js
+++ packages/common/jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.test.ts']
+  testMatch: ['**/tests/**/*.test.ts'],
+  moduleNameMapper: {
+    '^p-limit$': '<rootDir>/../../apps/vscode-extension/__mocks__/p-limit.js',
+    '^yocto-queue$': '<rootDir>/../../apps/vscode-extension/__mocks__/yocto-queue.js',
+  },
 };
